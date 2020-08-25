a1e40699334061d3129e23e65c5896cc77b7e727
kusano-lab <i_am_the_voice@docomo.ne.jp>
2020-08-25 23:56:02 +0900

    practice: q157~q160

--- my-app/practice/js/sample04.js
+++ my-app/practice/js/sample04.js
@@ -63,3 +63,46 @@
     // console.log('name', name) // name taro
     // console.log('twon', twon) // twon shibuya
 }
+
+
+// Q156
+{
+    const name = 'Taro'
+    const [a,,,b] = name;
+    // console.log('a', a) // a T
+    // console.log('b', b) // b o
+}
+
+
+// Q157,Q158
+{
+    let a = 1;
+    let b = 'goodby';
+    // console.log(a, b); // 1 'goodby'
+
+    b = [a, a = b][0]
+    // console.log(a, b) // goodby 1
+
+    // [a, b] = [b, a];
+    // console.log(a, b); // goodby 1
+}
+
+
+// Q159
+{
+    const input = [0,[1,2,3],4,5,[6]];
+    const inputB = [input[0], ...input[1], input[2], input[3], ...input[4]];
+    // console.log(inputB) // [ 0, 1, 2, 3, 4, 5, 6 ]
+}
+
+
+// Q160
+{
+    // document.addEventListener('DOMContentload', function(){
+    //     const target = document.getElementById('target')
+    //     target.textContent('change text')
+    // }, false)
+
+    //window.onload（load）は全てのloadが終わったタイミングで発火する
+    //DOMContentloadはDOM解析が終わってDOMに触れれるようになったら発火する　→ 画像読み込み前に実行可能。画像の幅などの処理にはloadを使用する
+}
